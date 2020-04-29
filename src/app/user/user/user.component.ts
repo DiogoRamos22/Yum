a6d91f6950de70a106d6dd885aa4f164c3978539
DiogoRamos22 <diogo22ramos@gmail.com>
2020-04-29 16:50:45 +0100

    User route added html and api

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/_services/api.service';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +9,37 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   userId: string;
+  imgUrl: any;
+  firstName: any;
+  lastName: any;
+  currentUserLocal: any;
+  birth: any;
+  gender: any;
+  address: any;
+  district: any;
+  county: any;
+  nickname: any;
+  email: any;
+  card: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe( params => {
       this.userId = params.userId;
+      this.api.userInfo(this.userId)
+        .then( res => {
+          this.imgUrl = res.data.image;
+          this.firstName = res.data.firstName;
+          this.lastName = res.data.lastName;
+          this.birth = res.data.birth;
+          this.gender = res.data.gender;
+          this.district = res.data.district;
+          this.email = res.data.email;
+        })
+        .catch( err => {
+          console.log(err);
+        })
   });
   }
 
